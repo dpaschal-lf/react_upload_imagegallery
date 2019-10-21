0fa9de8278f766084e426b8fd82a2c0121bae14c
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-10-20 20:53:04 -0700

    added build files

---
+++ build/precache-manifest.6579516cd2ac74de4bba259c7d0cedb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c5866e1c6f98d1f4687563cd2337b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abdcffad9a7db220d335",
+    "url": "/static/css/main.d45622c7.chunk.css"
+  },
+  {
+    "revision": "5e4261ebc13968cdb835",
+    "url": "/static/js/2.d479d45f.chunk.js"
+  },
+  {
+    "revision": "abdcffad9a7db220d335",
+    "url": "/static/js/main.05ac12eb.chunk.js"
+  },
+  {
+    "revision": "fefec75a69f9fef44631",
+    "url": "/static/js/runtime-main.e7b7cd36.js"
+  }
+]);
